8225c06b3027c6f1e5213131abb4f77a70295f78
Jovdat7 <151205453+Jovdat7@users.noreply.github.com>
2025-08-03 21:47:13 +0400

    Create prank.js

---
+++ prank.js
@@ -0,0 +1,6 @@
+var x = document.getElementsByTagName("div");
+var length = x.length;
+for (var i = 0; i < length; i++) {
+    x[i].style.fontSize = "100px";
+    x[i].innerHTML = "I LOVE JUSTIN BIEBER";
+}
